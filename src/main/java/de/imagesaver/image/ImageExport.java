51289d9abdc6f3b038f3ca4ed0fa2daca085f56d
Benno Kohrs <bennokohrs@gmail.com>
2020-02-11 19:10:23 +0100

    Added explimation to the functions

--- src/main/java/de/imagesaver/image/ImageExport.java
+++ src/main/java/de/imagesaver/image/ImageExport.java
@@ -14,16 +14,43 @@ public class ImageExport
     AES aes;
     ByteImageSaver byteImageSaver;
 
+    /**
+     * You can enable the Log and with that you can see for example if the export has been ended and so on..
+     *
+     * @param showLog Sets whether the log should be shown or not
+     */
+    public void setShowLog(boolean showLog)
+    {
+        this.showLog = showLog;
+    }
+
     //==================================================================================================================
-    //Instances
+    //Class
     //==================================================================================================================
+
+    /**
+     * You can export/import Strings/Objects/Files into images
+     *
+     * @param allowCrypting Sets whether the image should be encrypted or not
+     * @param color         Sets whether the image should be colorful or not (Note: The image without colors needs more space)
+     */
     public ImageExport(boolean allowCrypting, boolean color)
     {
         this.allowCrypting = allowCrypting;
         aes = new AES();
         byteImageSaver = new ByteImageSaver(showLog, !color);
+
+        StringBuilder builder = new StringBuilder();
+
     }
 
+    /**
+     * You can export/import Strings/Objects/Files into images
+     *
+     * @param allowCrypting Sets whether the image should be encrypted or not (Note: The KEY is useless if you do not allow crypting)
+     * @param KEY           A 16 byte StringKey which you have to define for encrypting your Data (Note: Only with that key you can decrypt your encrypted data)
+     * @param color         Sets whether the image should be colorful or not (Note: The image without colors needs more space)
+     */
     public ImageExport(boolean allowCrypting, String KEY, boolean color)
     {
         this.allowCrypting = allowCrypting;
@@ -31,6 +58,11 @@ public class ImageExport
         byteImageSaver = new ByteImageSaver(showLog, !color);
     }
 
+    /**
+     * You can export/import Strings/Objects/Files into images
+     *
+     * @param color Sets whether the image should be colorful or not (Note: The image without colors needs more space)
+     */
     public ImageExport(boolean color)
     {
         aes = new AES();
@@ -40,6 +72,14 @@ public class ImageExport
     //==================================================================================================================
     //Object
     //==================================================================================================================
+
+    /**
+     * Will export a Object into a image which will contain the data of the Object
+     * Note: Your Object has to be Serializable(java.io.Serializable)
+     *
+     * @param objects   The object which you will export as an image
+     * @param imagePath The path where the image should be saved
+     */
     public void exportObject(ArrayList<Object> objects, String imagePath)
     {
         //Object to ByteArray
@@ -53,6 +93,13 @@ public class ImageExport
         byteImageSaver.createImage(byteArray, imagePath);
     }
 
+    /**
+     * Will import a Object which has been read out of a image
+     * Note: Your Object has to be Serializable(java.io.Serializable)
+     *
+     * @param imagePath The path where the image is located
+     * @return an Object that is read from the picture
+     */
     public ArrayList<Object> importObject(String imagePath)
     {
         //ReadImage
@@ -70,6 +117,13 @@ public class ImageExport
     //==================================================================================================================
     //String
     //==================================================================================================================
+
+    /**
+     * Will export a String into a image which will contain the data of the String
+     *
+     * @param value     The string which you will export as an image
+     * @param imagePath The path where the image should be saved
+     */
     public void exportString(String value, String imagePath)
     {
         //String to ByteArray
@@ -84,6 +138,12 @@ public class ImageExport
         byteImageSaver.createImage(byteArray, imagePath);
     }
 
+    /**
+     * Will import a String which has been read out of a image
+     *
+     * @param imagePath The path where the image is located
+     * @return an String that is read from the picture
+     */
     public String importString(String imagePath)
     {
         //ReadImage
@@ -101,6 +161,13 @@ public class ImageExport
     //==================================================================================================================
     //File
     //==================================================================================================================
+
+    /**
+     * Will export a File into a image which will contain the data of the File
+     *
+     * @param filePath  The path where your file is located which you will export as an image
+     * @param imagePath The path where the image should be saved
+     */
     public void exportFile(String filePath, String imagePath)
     {
 
@@ -128,6 +195,12 @@ public class ImageExport
         byteImageSaver.createImage(byteArray, imagePath);
     }
 
+    /**
+     * Will import a String which has been read out of a image
+     *
+     * @param imagePath   The path where the image is located
+     * @param newFilePath The path where the decrypted file should be saved
+     */
     public void importFile(String newFilePath, String imagePath)
     {
         //ReadImage
@@ -150,6 +223,9 @@ public class ImageExport
         logString("Finish!");
     }
 
+    //==================================================================================================================
+    //Output
+    //==================================================================================================================
     private void logString(String log)
     {
         if (showLog)
